c95798ebabdb8df5d0e3635ae27c49915f75867a
otm0937 <otm0937@naver.com>
2026-02-26 11:11:45 +0900

    Init

---
+++ src/main/kotlin/gdfs/travelshop/TravelShopApplication.kt
@@ -0,0 +1,11 @@
+package gdfs.travelshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<TravelShopApplication>(*args)
+}
---
+++ src/test/kotlin/gdfs/travelshop/TravelShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package gdfs.travelshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TravelShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
